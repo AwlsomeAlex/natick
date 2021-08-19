8012682ecbd1f14a8a6d1be3be8f0e559c567b82
AwlsomeAlex <alex@awlsome.com>
2021-08-19 00:27:48 -0400

    test

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#=======================#
+# natickOS Build Script #
+#-----------------------#
+# ISC License           #
+#=======================#
+# Copyright (C) 2017-2021 AJ Barris (AwlsomeAlex)
+# alex at awlsome dot com
+# All Rights Reserved
+#=======================#
