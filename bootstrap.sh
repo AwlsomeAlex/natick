0c5db8a745fee9fe639dba2189a6444b52269eed
AwlsomeAlex <alex@awlsome.com>
2024-07-19 21:31:32 -0400

    Reboot?
    Signed-off-by: AwlsomeAlex <alex@awlsome.com>

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# natick GCC bootstrap
+# ====================
+# Copyright (C) 2017 - 2024 Alexander Barris
+# Licensed under ISC License
+# All Rights Reserved
+
+# ==== Global Variables ==== #
+EXE=${0}
+CMD=${1}
+ARG=${2}
+
+# ==== Script Functions ==== #
+
+# show_usage(): Show script usage
+function show_usage() {
+    echo "TODO"
+}
